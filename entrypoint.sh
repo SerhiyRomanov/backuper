2df3cb78315e5e6a2f2a8da96bba01099455886a
Serhii Romanov <sromanov@sharpminds.com>
2025-09-22 23:11:14 +0300

    Fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 CONFIG_FILE="/app/config.yaml"
 CRON_SCHEDULE=$(yq '.cron_schedule' $CONFIG_FILE)
 
-echo "$SCHEDULE root python3 /app/backup.py /app/config.yaml >> /var/log/backuper/cron_stdout.log 2>&1" > /etc/cron.d/pg_backuper
+echo "$SCHEDULE root python3 /app/backup.py /app/config.yaml >> /var/log/backuper/cron_stdout.log 2>&1" > /etc/cron.d/backupjob
 chmod 0644 /etc/cron.d/backupjob
 crontab /etc/cron.d/backupjob
 
