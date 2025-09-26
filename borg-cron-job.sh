b89ef454479c96628bba42d311e28512d03578e2
Serhii Romanov <sromanov@sharpminds.com>
2025-09-26 16:44:10 +0300

    Borg fixes

--- borg-cron-job.sh
+++ borg-cron-job.sh
@@ -13,19 +13,17 @@ exec 2>&1
 
 echo "###### Backup started: $(date) ######"
 
-export REPOSITORY_PATH=$(yq '.repository_path' "$CONFIG_FILE")
-export REPOSITORY_PORT=$(yq '.repository_port' "$CONFIG_FILE")
-export REPOSITORY_NAME=$(yq '.repository_name' "$CONFIG_FILE")
-export PRIVATE_KEY_PATH=$(yq '.private_key_path' "$CONFIG_FILE")
-
+export REPOSITORY_PATH=$(yq -r '.repository_path' "$CONFIG_FILE")
+export REPOSITORY_PORT=$(yq -r '.repository_port' "$CONFIG_FILE")
+export REPOSITORY_NAME=$(yq -r '.repository_name' "$CONFIG_FILE")
+export PRIVATE_KEY_PATH=$(yq -r '.private_key_path' "$CONFIG_FILE")
+export BORG_PASSPHRASE=$(yq -r '.repository_passphrase' "$CONFIG_FILE")
 export REPO=$REPOSITORY_PATH:$REPOSITORY_PORT/./$REPOSITORY_NAME
 
-export BORG_PASSPHRASE=$(yq '.repository_passphrase' "$CONFIG_FILE")
 export BORG_RSH="ssh -i ${PRIVATE_KEY_PATH}"
 
-
-export INCLUDE_FOLDERS=$(yq '.include_folders | join(" ")' "$CONFIG_FILE")
-export EXCLUDE_FOLDERS=$(yq '.exclude_folders | map("--exclude " + .) | join(" ")' "$CONFIG_FILE")
+export INCLUDE_FOLDERS=$(yq -r '.include_folders | join(" ")' "$CONFIG_FILE")
+export EXCLUDE_FOLDERS=$(yq -r '.exclude_folders | map("--exclude " + .) | join(" ")' "$CONFIG_FILE")
 
 
 echo "Transfer files ..."
